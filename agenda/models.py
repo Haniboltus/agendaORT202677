29c5fc8c35f9919cf9b69067c8d363e7d3b74009
prof-salu <saluorodrigues@gmail.com>
2026-05-26 15:38:18 -0300

    Models e Admin

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+#ORM --> Object-Relational Mapper
+
+#Contato
+    #nome
+    #telefone
+    #endereco
+    #email
+    #data_nascimento
+
+class Contato(models.Model):
+    #Criando o campo nome, com max de 255 caracteres, preenchimento obrigatorio
+    nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15, null=True, blank=True)
+    endereco = models.TextField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nome} - {self.email}'
