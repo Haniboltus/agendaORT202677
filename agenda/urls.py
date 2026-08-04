1874a812ee07a7f07b605c29173ce084e5aaf364
prof-salu <saluorodrigues@gmail.com>
2026-08-04 15:51:38 -0300

    Templates Dinâmicos ORM (Listando Contatos) ModelForm POST

--- agenda/urls.py
+++ agenda/urls.py
@@ -7,5 +7,7 @@ app_name = 'agenda'
 urlpatterns = [
     path('teste/', views.ola_mundo, name='ola_mundo'),
     path('', views.pagina_inicial, name='home'),
+    path('contatos/', views.contato_lista, name='contato_lista'),
+    path('contatos/novo', views.contato_criar, name='contato_criar')
 ]
 
--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Contato
+from .forms import ContatoForm
 
 # Create your views here.
 def ola_mundo(request):
@@ -7,3 +9,21 @@ def ola_mundo(request):
 
 def pagina_inicial(request):
     return render(request, 'agenda/index.html')
+
+#Retorna todos os contatos da agenda
+def contato_lista(request):
+    #ORM (Object-Relational Mapping)
+    contatos = Contato.objects.all()
+    #Renderiza o html passando a lista de contatos
+    return render(request, 'agenda/contatos_lista.html', {'contatos' : contatos})
+
+def contato_criar(request):
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agenda:contato_lista')
+    else:
+        form = ContatoForm()
+
+    return render(request, 'agenda/contato_form.html', {'form' : form})
